33a81e380ee60eb09d5888773f3ea4194f413afc
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-11-06 10:48:53 +0300

    Практика 1

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Main.kt
@@ -1,6 +1,27 @@
 package ru.tbank.education.school.lesson1
 
 fun main() {
-    print("Hello world")
+    print("Here comes the moneeeey")
     println("Hello World!")
+    val a = 1
+    val q: Short? = null // значение может быть нулем (указали тип Short)
+    val b = "cccc"
+    val c = 's'
+    val d = 1.23
+    val e = true
+    val f = "hello $d  world"
+    var g = 1 // в отличии от val ожно менять значение
+    g = 2
+    // Test
+    val o = "abcd $d"
+    println(o)
+    var k = 1
+    k += 1
+    println(k)
+    if (q == null) {
+        println("$q is null")
+    }
+
+
+
 }
