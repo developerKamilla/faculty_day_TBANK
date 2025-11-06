5972606e72088bc8844a944263e7dbe612a4d19a
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-11-06 11:15:27 +0300

    Сумма четных(улучшенная версия)

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/EvenNumbers.kt
@@ -1,11 +1,4 @@
 package ru.tbank.education.school.lesson1
 
-fun sumEvenNumbers(numbers: Array<Int>): Int {
-    var kol = 0
-    for (elem in numbers) {
-        if (elem % 2 == 0) {
-            kol += 1
-        }
-    }
-    return kol
-}
+fun sumEvenNumbers(numbers: Array<Int>) =
+    numbers.filter { it % 2 == 0}.sum()
