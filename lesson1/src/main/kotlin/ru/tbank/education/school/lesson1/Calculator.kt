063e5e287227d1573a936650427694b91687f7f7
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2025-11-11 20:47:36 +0300

    Решённые задачи String и calculator

--- lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
+++ lesson1/src/main/kotlin/ru/tbank/education/school/lesson1/Calculator.kt
@@ -1,18 +1,41 @@
 package ru.tbank.education.school.lesson1
 
-/**
- * Метод для вычисления простых арифметических операций.
- */
 fun calculate(a: Double, b: Double, operation: OperationType): Double? {
-    TODO()
+    if (b == 0.0) return null
+    else
+        if (operation == OperationType.ADD) {
+            return a + b
+        }
+        if (operation == OperationType.SUBTRACT) {
+            return a - b
+        }
+        if (operation == OperationType.DIVIDE) {
+            return a / b
+        }
+        if (operation == OperationType.MULTIPLY) {
+            return a * b
+        }
+    return null
 }
 
-/**
- * Функция вычисления выражения, представленного строкой
- * @return результат вычисления строки или null, если вычисление невозможно
- * @sample "5 * 2".calculate()
- */
 @Suppress("ReturnCount")
 fun String.calculate(): Double? {
-    TODO()
+    val word = this.split(Regex("\\s+"))
+    if (word.size != 3) return null
+    val v1 = word[0].toDoubleOrNull() ?: return null
+    val v2 = word[1]
+    val operation = when (v2) {
+        "+" -> OperationType.ADD
+        "-" -> OperationType.SUBTRACT
+        "*" -> OperationType.MULTIPLY
+        "/" -> OperationType.DIVIDE
+        else -> return null
+    }
+    val v3 = word[2].toDoubleOrNull() ?: return null
+    return calculate(v1, v3, operation)
+}
+
+fun main() {
+    println(calculate(10.0, 2.0, OperationType.MULTIPLY))
+    println("10 + unknown".calculate())
 }
