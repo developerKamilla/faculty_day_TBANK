b4a402c3c793ce3123ac8800d108c93066b7113b
developerKamilla <135846169+developerKamilla@users.noreply.github.com>
2026-01-21 21:47:10 +0300

    Домашнее задание Архиватор

--- lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/Main.kt
+++ lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/Main.kt
@@ -1,5 +1,21 @@
-package ru.tbank.education.school.lesson2
+interface DeploymentEmployee {
+    fun deployToProduction()
+}
 
-fun main() {
-    println("faculty day is the best course!")
+class Developer(private val name: String) : TBankEmployee, CodeWritingEmployee, DeploymentEmployee {
+    override fun writeCode() {
+        println("$name пишет код")
+    }
+
+    override fun answerClientCall() {
+        // Разработчик не отвечает на звонки клиентов
+    }
+
+    override fun processLoanRequest() {
+        // Разработчик не обрабатывает заявки на кредиты
+    }
+
+    override fun deployToProduction() {
+        println("$name деплоит код в продакшен")
+    }
 }
--- lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/tasks/9_i1/TBankEmployee.kt
+++ lesson3/src/main/kotlin/ru/tbank/education/school/lesson3/tasks/9_i1/TBankEmployee.kt
@@ -1,3 +1,5 @@
+package ru.tbank.education.school.lesson2
+
 interface TBankEmployee{
     fun writeCode()
     fun deployToProduction()
@@ -14,17 +16,53 @@ fun main() {
     val dev = Developer("Алексей")
     val support = SupportOperator("Мария")
     val loanManager = LoanManager("Игорь")
-
     // В реальности:
     // - разработчик не должен рассматривать кредиты
     // - оператор поддержки не должен деплоить в прод
     // - кредитный менеджер не должен писать код, и т.д.
     dev.writeCode()
     dev.processLoanRequest()
-
     support.answerClientCall()
     support.deployToProduction()
-
     loanManager.processLoanRequest()
     loanManager.writeCode()
 }
+
+
+package ru.tbank.education.school.lesson2
+interface TBankEmployee {
+    fun answerClientCall()
+}
+interface CodeWritingEmployee {
+    fun writeCode()
+}
+interface DeploymentEmployee {
+    fun deployToProduction()
+}
+interface LoanEmployee {
+    fun processLoanRequest()
+}
+class Developer(private val name: String) : TBankEmployee, CodeWritingEmployee {
+    override fun writeCode()
+    override fun answerClientCall()
+    fun deployToProduction()
+}
+class SupportOperator(private val name: String) : TBankEmployee, LoanEmployee {
+    override fun answerClientCall()
+    override fun processLoanRequest()
+}
+class LoanManager(private val name: String) : TBankEmployee, LoanEmployee , CodeWritingEmployee {
+    override fun answerClientCall()
+    override fun processLoanRequest()
+    fun deployToProduction()
+
+}
+fun main() {
+    val dev = Developer("Алексей")
+    val support = SupportOperator("Мария")
+    val loanManager = LoanManager("Игорь")
+    dev.writeCode()
+    support.answerClientCall()
+    loanManager.processLoanRequest()
+}
+
